cb380fd486caae1fd387e06ab634a61fcb26a070
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-05-12 19:59:54 +0530

    feat: Setup vitest & rtl

---
+++ src/__tests__/setup.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import matchers from "@testing-library/jest-dom/matchers";
+
+expect.extend(matchers);
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
@@ -12,5 +13,10 @@ export default defineConfig({
         replacement: path.resolve(__dirname, "./src"),
       },
     ],
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/__tests__/setup.ts",
   },
 });
