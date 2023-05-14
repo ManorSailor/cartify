9ea2cbe1e5173f62e1b4b960b501ec4de86b0562
ManorSailor <53308129+ManorSailor@users.noreply.github.com>
2023-05-14 23:00:08 +0530

    feat: Implement Home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { ReactElement } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+import { Home } from "./pages";
 
 function App(): ReactElement {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
